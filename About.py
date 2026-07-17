cdf38140dc3b1396ccd99fca7de565b1bfd04d2e
Hiroshi Sasaki <hasaki@softmatters.net>
2026-07-17 23:10:34 +0900

    一旦完成

--- About.py
+++ About.py
@@ -10,18 +10,37 @@ st.set_page_config(
 st.title('ここでできること')
 st.markdown("---")
 
-st.header("「応力緩和測定」の結果処理")
-st.subheader('１．測定結果の読み込み')
-st.subheader('２．適正範囲の選択')
-st.subheader('３．応力緩和グラフの表示')
-st.subheader('４．ReSpectを用いたデータ処理')
+st.header('pyReSpect-timeによる応力緩和関数$G(t)$から応力緩和スペクトル（連続）への変換')
+st.subheader('応力緩和関数$G(t)$から応力緩和スペクトル$\\mathcal{H}(s)$')
+st.markdown('応力緩和関数$G(t)$は、以下のように連続的な応力緩和スペクトル$\\mathcal{H}(s)$の積分表現、あるいは、一般化マックスウェルモデルとして離散的な緩和時間$\\tau_i$を有する緩和強度$g_i$のサンメンションとして表される')
+st.markdown(r"""
+            $$
+            \begin{align}
+            G(t) &= G_0 + \int_{-\infty}^{\infty} \mathcal{H}(s) e^{-t/s} d \ln s \cdots \text{連続表現} \\
+            G(t) &= G_0 + \sum_{i=1}^{N} g_i e^{-t/\tau_i} \cdots \text{離散表現}
+            \end{align}
+            $$
+            """)
+
+st.subheader('pyReSpect-timeとは')
+st.markdown('pyReSpect-timeは、数値シミュレーションにより上記の関係の逆問題を解くことで連続的な応力緩和スペクトル$\\mathcal{H}(s)$を導出し、その結果から一般化マックスウェルモデルの離散的な緩和時間$\\tau_i$を有する緩和強度$g_i$の組を求めるpythonスクリプトである')
+st.markdown('- 連続スペクトル$\\mathcal{H}(s)$は、「チホノフ正則化 (Tikhonov Regularization)」手法を用いたベイズ推定により求めている\n- 一般化マックスウェルモデルはAIC最小化により導出している')
+st.markdown('[pyReSpect-timeのサイト](https://github.com/shane5ul/pyReSpect-time)')
+st.subheader('参考文献')
+st.markdown('> Shanbhag, S., "pyReSpect: A Computer Program to Extract Discrete and Continuous Spectra from Stress Relaxation Experiments", Macromolecular Theory and Simulations, 2019, 1900005.')
+st.markdown('> Takeh, A. and Shanbhag, S., "A computer program to extract the continuous and discrete relaxation spectra from dynamic viscoelastic measurements", Applied Rheology, 2013, 23, 24628.')
+st.markdown("---")
+
+st.header("ここでの処理の流れ")
+st.subheader('処理の流れを以下に示した')
+st.markdown('1. 測定結果の読み込み\n1. 適正範囲の選択\n1. 応力緩和グラフの表示\n1. pyReSpectを用いたデータ処理')
 st.markdown("---")
 
 st.header("具体的な処理プロセス")
 st.subheader('左のメニューバーから順次処理を選択')
-st.subheader('メニューバーの内容説明は以下のタグで見てください')
+st.write('メニューバーのそれぞれのページでの処理内容についての説明は以下のタグで見てください')
 
-tab1, tab2,tab3, tab4, tab5 = st.tabs(['Data_Upload', 'Select_Columns', 'Modify_Data', 'Plot_Data', 'Run ReSpect'])
+tab1, tab2,tab3, tab4, tab5 = st.tabs(['Data_Upload', 'Select_Columns', 'Modify_Data', 'Plot_Data', 'Run_pyReSpect'])
 with tab1:
     st.subheader('このページでは、処理対象のデータをアップロードします')
     st.write('・必要に応じてオプションを選択')
@@ -48,6 +67,7 @@ with tab4:
     st.write('・内容に問題なければ「ボタンをクリック」')
 with tab5:
     st.subheader('このページでは、ここまでの処理結果をReSpectに入力して、データ処理を行います')
+    st.write('・必要に応じてオプションを選択')
     st.write('・「データ処理」のボタンをクリック')
     st.write('・「処理中」の表示を経て、「緩和関数」を「連続」と「離散」で求めた結果が表示されます')
     st.write('・同時に、上記の推定値による応力緩和グラフを測定値と合わせて表示されます')
--- pages/5_Run_ReSpect.py
+++ pages/5_Run_ReSpect.py
@@ -6,6 +6,8 @@ from pyrespect_time import ReSpect, ReSpectConfig
 import io
 import zipfile
 from datetime import datetime
+import os
+import tempfile
 ###
 def main():
 	st.set_page_config(
@@ -18,17 +20,129 @@ def main():
 		st.write('データはまだ選択されていません')
 	else:
 		st.success('有効なデータフレームが作成されています')
-		if st.button('データ処理しますか？'):
-			st.success('処理中です')
-			run_respect(st.session_state.cut_df)
+		make_config()
 	return
 
-def run_respect(df):
+def make_config():
+	## Default Values ##
+	# Continuous spectrum
+	ns = 100
+	plateau = False
+	freq_end = "lenient"
+	# Regularization
+	lam_min = 1e-10
+	lam_max = 1e3
+	lam_C = None
+	lam_density = 2
+	SmFacLam = 0.0
+    # Discrete spectrum
+	max_num_modes = None
+	delta_base_weight_dist = 0.2
+	min_tau_spacing = 1.25
+    # I/O
+	resample = True
+	n_resample = 100
+	##################
+	#
+	level = st.radio("データ処理に関するオプションを設定しますか？", ("Default", "Simple", "Detailed"), horizontal=True)
+	if level == 'Default':
+		if st.button('オプションを設定しないでデータ処理するならクリック', key='respect'):
+			config = ReSpectConfig(ns, plateau, freq_end, lam_min, lam_max, lam_C, lam_density, SmFacLam, max_num_modes, delta_base_weight_dist, min_tau_spacing, resample, n_resample)
+			with st.spinner('処理中です。しばらくお待ちください…'):
+				run_respect(st.session_state.cut_df, config)
+	elif level == 'Simple':
+		if st.checkbox('全体に関するオプションを設定しますか？', key='whole'):
+			ns = st.number_input('入力したデータの間引き後のデータ数', min_value=100, max_value=1000, step=100)
+			r_plateau = st.radio("ラバープラトーの有無？", ("No", "Yes"))
+			if r_plateau == 'No':
+				plateau = False
+			else:
+				plateau = True
+		if st.checkbox('出力に関するオプションを設定しますか？', key='out'):
+			out_res = st.radio("出力データの調整の有無？", ("Yes", "No"))
+			if out_res == 'No':
+				resample = False
+			else:
+				resample = True
+				n_resample = st.number_input('データポイント数', min_value=100, max_value=1000, step=100)
+		#
+		if st.button('オプション入力を終了してデータ処理しますか？', key='Simple'):
+			config = ReSpectConfig(ns, plateau, freq_end, lam_min, lam_max, lam_C, lam_density, SmFacLam, max_num_modes, delta_base_weight_dist, min_tau_spacing, resample, n_resample)
+			with st.spinner('処理中です。しばらくお待ちください…'):
+				run_respect(st.session_state.cut_df, config)
+	else:
+		# Continuous spectrum
+		if st.checkbox('全体に関するオプションを設定しますか？', key='whole'):
+			ns = st.number_input('入力したデータの間引き後のデータ数', min_value=100, max_value=1000, step=100)
+			r_plateau = st.radio("ラバープラトーの有無？", ("No", "Yes"))
+			if r_plateau == 'No':
+				plateau = False
+			else:
+				plateau = True
+			freq_end = st.radio("s軸の終端をどのように設定するのかを選択", ("lenient", "neutral", "strict"), horizontal=True)
+		if st.checkbox('「正則化」に関するオプションを設定しますか？', key='cont'):
+			sel_lam_min = st.radio(r"s軸の下限のデフォルト値は1e-10ですが、変更しますか？", ("No", "Yes"), horizontal=True)
+			if sel_lam_min == 'Yes':
+				lam_min = 10.**(st.slider('変更値', -11, -9, -10, 1, key='lam_min'))
+			else:
+				lam_min = 1e-10
+			sel_lam_max = st.radio(r"s軸の上限のデフォルト値は1e3ですが、変更しますか？", ("No", "Yes"), horizontal=True)
+			if sel_lam_max == 'Yes':
+				lam_max = 10.**(st.slider('変更値', 2, 4, 3, 1, key='lam_max'))
+			else:
+				lam_max = 1e3
+			sel_lam_C = st.radio("正則化パラメタ$\\lambda$のデフォルト値はNoneですが、変更しますか？", ("No", "Yes"), horizontal=True)
+			if sel_lam_C == 'Yes':
+				lam_C = st.slider('ベイズ最適化でのスムース性の調整', -1., 1., 0., .1, key='lam_C')
+			else:
+				lam_C = None
+			sel_lam_density = st.radio("Lカーブ探索グリッド中の$\\lambda$の密度のデフォルト値は2ですが、変更しますか？", ("No", "Yes"), horizontal=True)
+			if sel_lam_density == 'Yes':
+				lam_density = st.slider('Lカーブ探索グリッド中の$\\lambda$の密度', 1, 3, 2, 1, key='lam_density')
+			else:
+				lam_density = 2
+			sel_SmFacLam = st.radio("ベイズ最適化でのスムース性の調整のデフォルト値は0.0ですが、変更しますか？", ("No", "Yes"), horizontal=True)
+			if sel_SmFacLam == 'Yes':
+				SmFacLam = st.slider('ベイズ最適化でのスムース性の調整', -1.0, 1.0, 0.0, 0.1, key='smfaclam')
+			else:
+				SmFacLam = 0.0
+		if st.checkbox('離散処理に関するオプションを設定しますか？', key='disc'):
+			sel_max_num_modes = st.radio("一般化マックスウェルモデルのn数のデフォルト値はNoneで制限がありませんが、変更しますか？", ("No", "Yes"), horizontal=True)
+			if sel_max_num_modes == 'Yes':
+				max_num_modes = st.slider('一般化マックスウェルモデルのn数の調整', 1, 10, 5, 1, key='max_num_modes')
+			else:
+				max_num_modes = None
+			sel_delta_base_weight_dist = st.radio("AIC探索の感度のデフォルト値は0.2ですが、変更しますか？", ("No", "Yes"), horizontal=True)
+			if sel_delta_base_weight_dist == 'Yes':
+				delta_base_weight_dist = st.slider('AIC探索感度の調整', 0.1, 1., 0.2, .1, key='delta_base_weight_dist')
+			else:
+				delta_base_weight_dist = 0.2
+			sel_min_tau_spacing = st.radio("隣接する緩和時間分割に関するデフォルト値は1.25ですが、変更しますか？", ("No", "Yes"), horizontal=True)
+			if sel_min_tau_spacing == 'Yes':
+				min_tau_spacing = st.slider('緩和時間分割感度の調整', 1.1, 3., 1.5, .1, key='min_tau_spacing')
+			else:
+				min_tau_spacing = 1.25
+		if st.checkbox('出力に関するオプションを設定しますか？', key='out'):
+			out_res = st.radio("出力データの調整の有無？", ("Yes", "No"))
+			if out_res == 'No':
+				resample = False
+			else:
+				resample = True
+				n_resample = st.number_input('データポイント数', min_value=100, max_value=1000, step=100)
+		#
+		if st.button('オプション入力を終了してデータ処理しますか？', key='Detailed'):
+			config = ReSpectConfig(ns, plateau, freq_end, lam_min, lam_max, lam_C, lam_density, SmFacLam, max_num_modes, delta_base_weight_dist, min_tau_spacing, resample, n_resample)
+			with st.spinner('処理中です。しばらくお待ちください…'):
+				run_respect(st.session_state.cut_df, config)
+
+	return
+
+def run_respect(df, config):
 	time = df['Mod. Time'][0:].to_numpy()
 	gt = df['G(t)'][0:].to_numpy()
 
 	# Default settings — fit from a data file
-	solver = ReSpect()
+	solver = ReSpect(config)
 	solver.fit(time, gt)  # "Gt.dat" file contains data
 
 	# Access results
